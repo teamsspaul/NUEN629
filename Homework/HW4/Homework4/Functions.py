c438d2bd1b060790fa79c14a5ca469d53d0124aa
Paul Mendoza <paul.m.mendoza@gmail.com>
2017-01-03 00:31:31 -0600

    "Update Files"

--- Homework/HW4/Homework4/Functions.py
+++ Homework/HW4/Homework4/Functions.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
 
-"""
-FractionAM converts atom fractions to mass fractions
-and mass fractions to atom fractions. Input is a 
-single string with MCNP style fractions.
-"""
-
-__author__     =  "Paul Mendoza"
-__copyright__  =  "Copyright 2016, Planet Earth"
-__credits__    = ["Sunil Chirayath",
-                  "Charles Folden",
-                  "Jeremy Conlin"]
-__license__    =  "GPL"
-__version__    =  "1.0.1"
-__maintainer__ =  "Paul Mendoza"
-__email__      =  "paul.m.mendoza@gmail.com"
-__status__     =  "Production"
-
 ################################################################
 ##################### Import packages ##########################
 ################################################################
