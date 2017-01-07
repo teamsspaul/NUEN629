c98e649c7b24b9718e35a7103072942d0880a8a3
Paul Mendoza <paul.m.mendoza@gmail.com>
2017-01-07 11:52:26 -0600

    "Update Files"

--- Homework/HW45P/Homework5/Functions.py
+++ Homework/HW45P/Homework5/Functions.py
@@ -490,6 +490,49 @@ def BackEuler(A,no,dt):
     #Change to matrix compatable
     return((1-A*dt)**-1*no)
 
+def DeterminePolesNResidues(n):
+
+    #function [zk,ck] = cf(n);
+    K = 75;                             # no of Cheb coeffs
+    nf = 1024;                          # no of pts for FFT
+
+    #Roots correct?
+    roots=np.arange(1,nf,1)/nf
+    #w = np.exp(2i*pi*(0:nf-1)/nf);     # roots of unity
+
+
+    
+    w=np.exp(2j*np.pi*roots)
+    t = np.real(w);                     # Cheb pts (twice over)
+    scl = 9;                            # scale factor for stability
+    #F = np.exp(scl*(t-1)./(t+1+1e-16)); # exp(x) transpl. to [-1,1]
+    F = np.exp(scl*(t-1)/(t+1+1e-16)); # exp(x) transpl. to [-1,1]
+    c = np.real(np.fft.fft(F))/nf;      # Cheb coeffs of F
+    print(c)
+    #f = np.polyval(c(K+1:-1:1),w);      # analytic part f of F
+    #print(f)
+    quit()
+    #[U,S,V] = svd(hankel(c(2:K+1)));    # SVD of Hankel matrix
+    #s = S(n+1,n+1);                     # singular value
+    #u = U(K:-1:1,n+1)’; v = V(:,n+1)’;  # singular vector
+    #zz = zeros(1,nf-K);                 # zeros for padding
+    #b = fft([u zz])./fft([v zz]);       # finite Blaschke product
+    #rt = f-s*w.^K.*b;                   # extended function r-tilde
+    #rtc = real(fft(rt))/nf;             # its Laurent coeffs
+    #zr = roots(v); qk = zr(abs(zr)>1);  # poles
+    #qc = poly(qk);                      # coeffs of denominator
+    #pt = rt.*polyval(qc,w);             # numerator
+    #ptc = real(fft(pt)/nf);             # coeffs of numerator
+    #ptc = ptc(n+1:-1:1); ck = 0*qk;
+
+    #N+1?
+    #for k in range(1:n+1):              # calculate residues
+    #    q = qk(k); q2 = poly(qk(qk~=q));
+    #    ck(k) = polyval(ptc,q)/polyval(q2,q);
+    #zk = scl*(qk-1).^2./(qk+1).^2;      # poles in z-plane
+    #ck = 4*ck.*zk./(qk.^2-1);           # residues in z-plane
+    return(ck,zk)
+
 def RationalPrep(N,Phi):
     """Calculate constants for a rational approximation
     Inputs:
@@ -514,6 +557,9 @@ def RationalPrep(N,Phi):
     elif Phi=='Hyperbola':
         zk=2.246*N*(1-np.sin(1.1721-0.3443j*theta))
         w=2.246*N*(0.3443j*np.cos(1.1721-0.3443j*theta))
+    elif Phi=='Best':
+        ck,zk=DeterminePolesNResidues(N)
+        return(ck,zk)
     else:
         print("Did not pick proper rational approximation dude")
         print("Quiting now")
